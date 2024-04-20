f85ee0c32a418071604dd9bacf7b813b9dc96ab9
RyanC8451 <ryan8401@bu.edu>
2024-04-19 21:04:43 -0400

    creating SecureSwap branch

--- SecureSwap.sol
+++ SecureSwap.sol
@@ -1,16 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.0 <0.9.0;
 
-contract A {
+contract SecureSwap {
     // ...
 }
 
-
-contract B {
-    // ...
-}
-
-
-contract C {
-    // ...
-}
