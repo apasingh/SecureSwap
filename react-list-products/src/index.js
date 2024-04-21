587299abcfd4413abea131e60c254dbc0f088c85
mmiah <mmiah@bu.edu>
2024-04-20 23:31:39 -0400

    code to compare two pictures

--- react-list-products/src/index.js
+++ react-list-products/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ImageComparison from "./verifyimages"; 
+import HomePage from "./components/HomePage"; 
+import "./components/HomePage.css"; 
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
@@ -13,6 +16,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ImageComparison/>
+
   </React.StrictMode>
 );
 
