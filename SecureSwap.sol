4664c7fc739d500ee77a50100dd75cd4d2b31e6c
RyanC8451 <ryan8401@bu.edu>
2024-04-28 15:37:12 -0400

    updated smart contract

--- SecureSwap.sol
+++ SecureSwap.sol
@@ -22,6 +22,22 @@ contract SecureSwap {
 
     mapping(address => uint) public balances;
 
+    mapping(address => uint) public stakedBalances;
+
+    // constructor(){
+    //   Product memory hardcodedProduct;
+    //   hardcodedProduct.id = 1;
+    //   hardcodedProduct.seller = payable(address(0xb05c2605f9D58B2c20E35e136Ef129a59c90F76c)); // Seller address
+    //   hardcodedProduct.description = "Sample product";
+    //   hardcodedProduct.price = 1e14; // Price in wei (0.0001 Sepolia ETH)
+    //   hardcodedProduct.sellerDeposit = 1e15; // Seller deposit in wei (0.001 Sepolia ETH)
+    //   hardcodedProduct.isSold = false;
+
+    //   products[hardcodedProduct.id] = hardcodedProduct;
+    //   productCount++;
+      
+    // }
+
     /*
      * This event is emitted when a new product is listed in the marketplace.
      * uint id: The unique identifier of the listed product.
@@ -37,6 +53,10 @@ contract SecureSwap {
      * uint price: The price at which the product was purchased.
      */
     event ProductPurchased(uint id, address buyer, uint price);
+
+
+     // Event emitted when a transaction is agreed upon
+    event TransactionAgreed(uint productId, address buyer, address seller, uint price);
 
      /*
       * This event is Emitted when a transaction is approved, indicating that both parties have agreed and the funds can be transferred to the seller.
@@ -54,34 +74,74 @@ contract SecureSwap {
      */
     event TransactionDisputed(uint id, string reason);
 
-    /*
-     * Function to list a new product
-     * String memory _description: This is a temperory memory storage between transaction
-     * uint _price: The price that the item is list for
-     * uint _sellerDesposit: The collatoral payment the seller need to put down
-     */
-    function listProduct(string memory _description, uint _price, uint _sellerDeposit) public {
-        require(_sellerDeposit > _price, "Deposit must be higher than price");
-        productCount++;
-        //This specifies where the new product will be stored in the market 
-        products[productCount] = Product(productCount, payable(msg.sender), _description, _price, _sellerDeposit, false);
-        //Notify people if the product is on the chain
-        emit ProductListed(productCount, msg.sender, _price);
+    // /*
+    //  * Function to list a new product
+    //  * String memory _description: This is a temperory memory storage between transaction
+    //  * uint _price: The price that the item is list for
+    //  * uint _sellerDesposit: The collatoral payment the seller need to put down
+    //  */
+    // function listProduct(string memory _description, uint _price, uint _sellerDeposit) public {
+    //     require(_sellerDeposit > _price, "Deposit must be higher than price");
+    //     productCount++;
+    //     //This specifies where the new product will be stored in the market 
+    //     products[productCount] = Product(productCount, payable(msg.sender), _description, _price, _sellerDeposit, false);
+    //     //Notify people if the product is on the chain
+    //     emit ProductListed(productCount, msg.sender, _price);
+    // }
+
+
+  // Function for buyers to agree to a transaction
+  function agreeToTransaction(uint _productId) public payable {
+      Product storage product = products[_productId];
+      require(product.seller != address(0), "Product does not exist");
+      require(product.isSold == false, "Product already sold");
+      require(msg.sender != product.seller, "Buyer cannot be the seller");
+
+      // Transfer the product price from the buyer to the contract
+      require(msg.value == product.price, "Send exact product price");
+
+      // Staking the seller's deposit
+      stakedBalances[product.seller] += product.sellerDeposit;
+
+      // Mark the product as sold
+      product.isSold = true;
+
+      // Emit event indicating the transaction agreement
+      emit TransactionAgreed(_productId, msg.sender, product.seller, product.price);
+    }
+
+    // Function for sellers to withdraw their staked balance
+    function withdrawStake() public {
+        uint amount = stakedBalances[msg.sender];
+        require(amount > 0, "No staked balance to withdraw");
+
+        // Transfer staked balance to the seller
+        stakedBalances[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
     }
 
     /*
      * Function for a buyer to purchase a product
      * unit _productId: The identifier of the purchase product.
      */
+    // function purchaseProduct(uint _productId) public payable {
+    //     //Find the item storeed in the marketplace
+    //     Product storage product = products[_productId];
+    //     require(msg.value == product.price, "Send exact product price");
+    //     require(product.isSold == false, "Product already sold");
+    //     product.isSold = true;
+    //     balances[product.seller] += msg.value;
+    //     emit ProductPurchased(_productId, msg.sender, product.price);
+    // }  
+
     function purchaseProduct(uint _productId) public payable {
-        //Find the item storeed in the marketplace
-        Product storage product = products[_productId];
-        require(msg.value == product.price, "Send exact product price");
-        require(product.isSold == false, "Product already sold");
-        product.isSold = true;
-        balances[product.seller] += msg.value;
-        emit ProductPurchased(_productId, msg.sender, product.price);
-    }  
+      Product storage product = products[_productId];
+      require(msg.value == product.price, "Send exact product price");
+      require(product.isSold == false, "Product already sold");
+      product.isSold = true;
+      balances[product.seller] += msg.value;
+      emit ProductPurchased(_productId, msg.sender, product.price);
+    }
 
     /*
      * Function to approve the transaction and release funds to the seller
@@ -107,4 +167,3 @@ contract SecureSwap {
     }
 
 }
-
