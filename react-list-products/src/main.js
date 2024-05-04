851a729ec350b07587efa14a8c9c04b27f92cb6e
mmiah <mmiah@bu.edu>
2024-05-04 14:52:45 -0400

    trying to connect to metamask

---
+++ react-list-products/src/main.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { Config, DAppProvider, Sepolia } from '@usedapp/core';
+import { MantineProvider } from "@mantine/core";
+//import '@mantine/core/dist/styles.min.css';
+import 'normalize.css';
+
+
+const config = {
+  readOnlyChainId: Sepolia.chainId,
+  readOnlyUrls: {
+    [Sepolia.chainId]: "https://sepolia.infura.io/v3/7018b8d34628491fb380004b171a78cb",
+  },
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+    <DAppProvider config={config}>
+      <MantineProvider>
+        <App />
+      </MantineProvider>
+    </DAppProvider>
+    
+  </React.StrictMode>,
+)
