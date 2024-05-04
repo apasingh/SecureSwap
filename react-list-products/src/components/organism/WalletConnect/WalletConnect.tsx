c42a97b3ca78dd6c87a4d33c1531a140424a2991
RyanC8451 <ryan8401@bu.edu>
2024-05-04 15:57:05 -0400

    organize files

---
+++ react-list-products/src/components/organism/WalletConnect/WalletConnect.tsx
@@ -0,0 +1,23 @@
+import { Button, Group, Text} from "@mantine/core";
+import { Sepolia, useEthers } from "@usedapp/core";
+import React from 'react';
+
+
+export const WalletConnect =() =>{
+    const { activateBrowserWallet, account, deactivate, chainId, switchNetwork} = useEthers(); //makes connection accessible in whole app
+    if (account) {
+        if (chainId === Sepolia.chainId){
+            return <Button onClick={deactivate}> Disconnect</Button>
+        } else{
+            return (
+                <Group style={{ justifyContent: 'center' }}>
+                <Text style={{ color: 'red' }}>Wrong Network</Text>
+            <Button onClick={() => switchNetwork(Sepolia.chainId)}> Switch Network</Button>
+            </Group>
+            );
+        }
+        
+    } else{
+    return <Button onClick={activateBrowserWallet}> Connect to Metamask </Button>
+}
+};
---
+++ react-list-products/src/components/organism/WalletInstallation/WalletInstallation.tsx
@@ -0,0 +1,20 @@
+import { Text, Title } from "@mantine/core";
+import React from 'react';
+
+export const WalletInstallation = () => {
+  return (
+    <>
+      <Title>MetaMask is required</Title>
+      <Text>
+        Follow the link to install
+        <Text
+          variant="link"
+          component="a"
+          href="https://metamask.io/download.html"
+        >
+           Meta Mask
+        </Text>
+      </Text>
+    </>
+  );
+};
